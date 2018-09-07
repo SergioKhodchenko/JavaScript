d80850933948c71ec93c0b81fd3e174019ef2832
Sergio <sirogxod@gmail.com>
2018-09-07 23:34:41 +0300

    remake

--- module_07/script.js
+++ module_07/script.js
@@ -34,33 +34,54 @@ const posts = [
 ];
 const body = document.querySelector('body');
 
-function createPostCard(post) {
-	const div = document.createElement('div');
-		div.classList.add('post');
-	const img = document.createElement('img');
+function createImage(post) {
+	const img = document.createElement('img'); //создаю картинку карточки
 		img.classList.add('post__image');
 		img.setAttribute('src', post.img);
 		img.setAttribute('alt', 'post image');
-	const h2 = document.createElement('h2');
+		return img;
+}
+function createTitle(post) {
+	const h2 = document.createElement('h2');  //создаю заголовок карточки
 		h2.classList.add('post__title');
 		h2.textContent = post.title;
-	const p = document.createElement('p');
+		return h2;
+}
+function createText(post) {
+	const p = document.createElement('p');   //создаю текст карточки
 		p.classList.add('post__text');
 		p.textContent = post.text;
-	const a = document.createElement('a');
+		return p;
+}
+function createLink(post) {
+	const a = document.createElement('a');     //создаю кнопку карточки
 		a.classList.add('button');
 		a.textContent = 'Read more';
 		a.setAttribute('href', post.link);
-	body.append(div);
-	div.append(img);
-	div.append(h2);
-	div.append(p);
-	div.append(a);
-}
-function createCards(posts) {
-	for (const post of posts) {
-    createPostCard(post);
-  }
+		return a;
 }
 
-createCards(posts);
+function createPostCard(post) {
+	const div = document.createElement('div');  //ф-ция создает и возвращает 1 готовую карточку 
+	div.classList.add('post');
+	const img = createImage(post);
+	const h2 = createTitle(post)
+	const p = createText(post);
+	const a = createLink(post);	
+	div.append(img, h2, p, a);
+	return div;          
+}
+function createCards(posts) {     
+	const allCards = posts.map(post => createPostCard(post));  //ф-ция возвращает массив готовых карточек
+	return allCards;                 
+}
+	
+const allCardsArray = createCards(posts);  // сохраняю массив готовых карточек, вызвав ф-цию и передав ей заданный нам объект "posts"
+
+
+const wrapper = document.createElement('div');
+for (const card of allCardsArray) {
+	wrapper.append(card); // добавляем каждую карточку из массива во wrapper
+}
+
+body.append(wrapper); //вставляем wrapper co всеми карточками в DOM за 1 раз
